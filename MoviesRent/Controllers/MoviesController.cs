9e5a949f3fa9a7235a81e110704bf3f44b5afa02
Birat Lamichhane <birat.lamichhane@selu.edu>
2019-06-09 02:16:09 -0500

    Setup database using Entitiy Framework

--- MoviesRent/Controllers/MoviesController.cs
+++ MoviesRent/Controllers/MoviesController.cs
@@ -10,6 +10,21 @@ namespace MoviesRent.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
         // GET: Movies
         public ActionResult Random()
         {
@@ -25,7 +40,7 @@ namespace MoviesRent.Controllers
                 Customers = customers
             };
 
-            return View(movie);
+            return View(viewModel);
         }
     }
 }
