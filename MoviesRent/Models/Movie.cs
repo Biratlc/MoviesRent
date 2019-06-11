2f6d8362b8d2b8896f03eccfd8ea33d389281563
Birat Lamichhane <birat.lamichhane@selu.edu>
2019-06-10 23:02:25 -0500

    Movieform created

--- MoviesRent/Models/Movie.cs
+++ MoviesRent/Models/Movie.cs
@@ -9,17 +9,23 @@ namespace MoviesRent.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
-        public string Name { get; set; }
+        public string Name { get; set; } 
+
+        [Display(Name = "Genre")]
         [Required]
         public Genre Genre { get; set; }
+
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Released Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Display(Name = "Number in Stock")]  
         public byte NumberInStock
         {
             get; set;
